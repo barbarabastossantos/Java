750ed95520d3345f76357ada740b377040b7ac24
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-04-11 22:07:45 -0300

    Incentivo...

---
+++ primeiro_programa/OlaMundo.java
@@ -0,0 +1,8 @@
+package primeiro_programa;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        
+        System.out.println("Barbara você é incrivel! Muito inteligente e vai conseguir ser uma programadora de sucesso!");
+    }
+}
